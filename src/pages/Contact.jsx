975e9bcb617836c54fc122b2ea57f3ee5171767d
HugoHtvl <hug.oo@hotmail.fr>
2023-08-17 17:49:28 +0200

    some modifications on contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -23,14 +23,13 @@ const Contact = () => {
         <div className="contactCards">
           <div className="infoContactCards">
             <img src={mailLogo} alt="Logo d'une enveloppe" />
-            <a href="mailto:hattenville.hugo@gmail.com" target="_blank">
-              hattenville.hugo@gmail.com
+            <a href="mailto:mon.mail@gmail.com" target="_blank">
+              mon.mail@gmail.com
             </a>
           </div>
-          <br />
           <div className="infoContactCards">
             <img src={phoneLogo} alt="Logo d'un téléphone" />
-            <p>06 49 81 75 79</p>
+            <p>06 06 06 06 06</p>
           </div>
         </div>
       </section>
